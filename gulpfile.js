fb397f5ade9c0b934201cbc65e9a3823417a8fec
danyn <danyn.com@gmail.com>
2018-04-27 13:09:37 -0700

    update build process to npm run serve

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,21 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
  
 gulp.task('sass', function () {
-  return gulp.src('./src/_sass/*.scss')
+  return gulp.src('./_sass/*.scss')
       .pipe(sass({
           outputStyle: 'compressed'
       }))
-      .pipe(gulp.dest('./src/_includes/css'));
+      .pipe(gulp.dest('./_includes/css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/_sass/*.scss', ['sass']);
+gulp.task('watch', function(){
+  gulp.watch('./_sass/*.scss', gulp.series('sass')); 
+  
 });
+
+gulp.task('default', gulp.series('sass', 'watch', function(){
+  console.log("running tasks");
+}));
