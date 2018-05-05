adc6a5592d9446cf07bb7d77d92d5eb79d996bdf
danyn <danyn.com@gmail.com>
2018-05-05 11:12:21 -0700

    gulpfile supports scss in component folders

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,43 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
- 
 gulp.task('sass', function () {
   return gulp.src('./_sass/*.scss')
       .pipe(sass({
           outputStyle: 'compressed'
+
       }))
       .pipe(gulp.dest('./_includes/css'));
 });
  
+gulp.task('sassPages', function () {
+  return gulp.src('./pages/**/**/*.scss')
+      .pipe(sass({
+          outputStyle: 'compressed',
+          includePaths: [
+            './_sass'
+        ]
+      }))
+      .pipe(gulp.dest('./_includes/css'));
+});
+
+gulp.task('sassComponents', function () {
+  return gulp.src('./_includes/**/**/*.scss')
+      .pipe(sass({
+          outputStyle: 'compressed',
+          includePaths: [
+            './_sass'
+        ]
+      }))
+      .pipe(gulp.dest('./_includes/css'));
+});
+
+
 gulp.task('watch', function(){
   gulp.watch('./_sass/*.scss', gulp.series('sass')); 
   
 });
 
-gulp.task('default', gulp.series('sass', 'watch', function(){
+gulp.task('default', gulp.series('sass','sassPages' , 'sassComponents' , 'watch', function(){
   console.log("running tasks");
 }));
